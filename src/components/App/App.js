5be7394acb39c02622878a27a7b5484347a6954f
Mnats Avetisyan <mnatsteam@mail.ru>
2022-08-16 00:45:57 +0300

    features/create-header

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "../Header/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
